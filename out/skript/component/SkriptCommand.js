1c451b816a74c4f7835d6d2e7a1d31a4816b5114
vhone <kiyouin16@gamil.com>
2021-06-24 17:27:35 +0900

    v0.0.7

--- out/skript/component/SkriptCommand.js
+++ out/skript/component/SkriptCommand.js
@@ -5,9 +5,16 @@ const vscode_1 = require("vscode");
 const SkriptComponent_1 = require("./SkriptComponent");
 const SkriptOptions_1 = require("./SkriptOptions");
 class SkriptCommand extends SkriptComponent_1.SkriptComponent {
-    constructor(skFile, range, docs, name, _body) {
-        super(skFile, range, docs, name);
+    constructor(skFile, range, docs, _header, _body) {
+        super(skFile, range, docs, _header.toString());
+        this._header = _header;
         this._body = _body;
+    }
+    get label() {
+        return this._header.label;
+    }
+    get argument() {
+        return this._header.argument;
     }
     get body() {
         return this._body;
@@ -32,9 +39,16 @@ class SkriptCommandBuilder extends SkriptComponent_1.SkriptComponentBuilder {
                         this._head = this._head.replace(`{@${key}}`, `${option.options.get(key)}`);
                     }
             }
-        // /^command\s(\/?[^\s]+)((?:\s+?[^\s\:]+)*)\s*?\:/i
-        let groups = (_a = this._head.match(/^command\s\/?(?<label>[^\s]+)(?<arguments>[^\:]*)/i)) === null || _a === void 0 ? void 0 : _a.groups;
-        console.log(groups);
+        let header = new SkriptCommandHeader(this._head);
+        let optionConfig = {};
+        for (const line of this._body.split(/\r\n|\r|\n/i)) {
+            let groups = (_a = line.match(/^(\t|\s{4})(?<option>aliases|description|usage|permission message|executable by|permission|cooldown|cooldown message|cooldown bypass|cooldown storage)\:\s*(?<value>.*)/i)) === null || _a === void 0 ? void 0 : _a.groups;
+            if (groups) {
+                // cmdOptions.set(groups.option, groups.value)
+            }
+        }
+        let skCommandOptions = new SkriptCommandOptions(optionConfig);
+        console.log(skCommandOptions);
         /*
          * 명령어 구체적으로 나누기
          * [label]
@@ -42,8 +56,37 @@ class SkriptCommandBuilder extends SkriptComponent_1.SkriptComponentBuilder {
          * [command option]
          * [trigger]
          */
-        return new SkriptCommand(this._skFile, this._range, this._docs, this._head, this._body);
+        return new SkriptCommand(this._skFile, this._range, this._docs, header, this._body);
     }
 }
 exports.SkriptCommandBuilder = SkriptCommandBuilder;
+class SkriptCommandOptions {
+    constructor(_config) {
+        this._config = _config;
+    }
+    get aliases() {
+        return this._config.aliases;
+    }
+}
+class SkriptCommandHeader {
+    constructor(_head) {
+        var _a;
+        this._head = _head;
+        let groups = (_a = this._head.match(/^command\s\/?(?<label>[^\s\:]+)(\s(?<argument>.*))?/i)) === null || _a === void 0 ? void 0 : _a.groups;
+        if (!groups)
+            throw new Error("SkriptCommandHeader: unmatch");
+        this._label = groups.label;
+        this._argument = groups.argument;
+    }
+    get label() {
+        return this._label;
+    }
+    get argument() {
+        return this._argument;
+    }
+    toString() {
+        return `command /${[this._label, this._argument].join(' ')}`;
+    }
+    ;
+}
 //# sourceMappingURL=SkriptCommand.js.map
--- src/skript/component/SkriptCommand.ts
+++ src/skript/component/SkriptCommand.ts
@@ -5,12 +5,19 @@ import { SkriptOptions } from "./SkriptOptions";
 
 export class SkriptCommand extends SkriptComponent {
 
-    constructor(skFile:SkriptFile, range: Range, docs: string[], name: string,
+    constructor(skFile:SkriptFile, range: Range, docs: string[],
+        private readonly _header: SkriptCommandHeader,
         private readonly _body: string
     ) {
-        super(skFile, range, docs, name);
+        super(skFile, range, docs, _header.toString());
     }
 
+    public get label(): string {
+        return this._header.label;
+    }
+    public get argument(): string | undefined {
+        return this._header.argument;
+    }
     public get body(): string {
         return this._body;
     }
@@ -34,9 +41,16 @@ export class SkriptCommandBuilder extends SkriptComponentBuilder<SkriptCommand> 
             }
         }
 
-        // /^command\s(\/?[^\s]+)((?:\s+?[^\s\:]+)*)\s*?\:/i
-        let groups = this._head.match(/^command\s\/?(?<label>[^\s]+)(?<arguments>[^\:]*)/i)?.groups;
-        console.log(groups);
+        let header = new SkriptCommandHeader(this._head);
+        let optionConfig: SkriptCommandOptionConfig = {};
+        for (const line of this._body.split(/\r\n|\r|\n/i)) {
+            let groups = line.match(/^(\t|\s{4})(?<option>aliases|description|usage|permission message|executable by|permission|cooldown|cooldown message|cooldown bypass|cooldown storage)\:\s*(?<value>.*)/i)?.groups;
+            if  (groups) {
+                // cmdOptions.set(groups.option, groups.value)
+            }
+        }
+        let skCommandOptions = new SkriptCommandOptions(optionConfig);
+        console.log(skCommandOptions);
 
         /* 
          * 명령어 구체적으로 나누기
@@ -46,6 +60,87 @@ export class SkriptCommandBuilder extends SkriptComponentBuilder<SkriptCommand> 
          * [trigger]
          */
 
-        return new SkriptCommand(this._skFile, this._range, this._docs, this._head, this._body);
+        return new SkriptCommand(this._skFile, this._range, this._docs, header, this._body);
     }
+}
+
+interface SkriptCommandOptionConfig {
+    aliases?: string;
+    description?: string;
+    usage?: string;
+    permission?: string;
+    permissionMessage?: string;
+    executableBy?: string;
+    cooldown?: string;
+    cooldownMessage?: string;
+    cooldownBypass?: string;
+    cooldownStorage?: string;
+}
+
+class SkriptCommandOptions {
+    
+    constructor(
+        private readonly _config: SkriptCommandOptionConfig) {
+    }
+
+    public get aliases() {
+        return this._config.aliases;
+    }
+    /*
+    public get description() {
+        return this._options.get('description')
+    }
+    public get usage() {
+        return this._options.get('usage')
+    }
+    public get permission() {
+        return this._options.get('permission')
+    }
+    public get permissionMessage() {
+        return this._options.get('permission message')
+    }
+    public get executableBy() {
+        return this._options.get('executable by')
+    }
+    public get cooldown() {
+        return this._options.get('cooldown')
+    }
+    public get cooldownMessage() {
+        return this._options.get('cooldown message')
+    }
+    public get cooldownBypass() {
+        return this._options.get('cooldown bypass')
+    }
+    public get cooldownStorage() {
+        return this._options.get('cooldown storage')
+    }
+    */
+
+}
+
+class SkriptCommandHeader {
+
+    private readonly _label: string;
+    private readonly _argument: string | undefined;
+
+    constructor(
+        private readonly _head: string
+    ) {
+        let groups = this._head.match(/^command\s\/?(?<label>[^\s\:]+)(\s(?<argument>.*))?/i)?.groups;
+        if (!groups)
+            throw new Error("SkriptCommandHeader: unmatch");
+        this._label = groups.label;
+        this._argument = groups.argument; 
+    }
+
+    public get label(): string {
+        return this._label
+    }
+    public get argument(): string | undefined {
+        return this._argument
+    }
+    public toString(): string {
+        return `command /${[this._label, this._argument].join(' ')}`
+    };
+
 }
