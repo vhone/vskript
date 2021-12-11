56b59cb45e7a268dd8d6a5a6112bad0d6ac05737
vhone <kiyouin16@gmail.com>
2021-12-11 20:12:46 +0900

    rederection

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,11 @@
 import { ExtensionContext, IndentAction, languages, workspace } from 'vscode';
 import { onSkriptEnable } from './skript_v0.0.7/Skript';
-import * as Provider from './provider/Provider';
-import TextDocumentChangeEvent from './event/TextDocumentChangeEvent';
-import { LEGEND } from './provider/SkriptDocumentSemanticTokensProvider';
+import * as Provider from './skript_v0.0.7/provider/Provider';
+import TextDocumentChangeEvent from './skript_v0.0.7/event/TextDocumentChangeEvent';
+import { LEGEND } from './skript_v0.0.7/provider/SkriptDocumentSemanticTokensProvider';
 import { VisualSkript } from './skript_v0.0.8/skript/Skript';
 import { FileParser } from './skript_v0.0.8/skript/parser/File';
 import * as FileSystem from 'fs';
-import * as VSCode from 'vscode';
-import * as FileUtils from './skript_v0.0.8/skript/Util/FileUtils'
 
 // Options
 languages.setLanguageConfiguration('vskript', {
