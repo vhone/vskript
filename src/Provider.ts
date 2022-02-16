2ff0207a5342f2042871925b4e2ec3a93df6fbbe
vhone <59043053+vhone@users.noreply.github.com>
2022-02-17 02:53:22 +0900

    Delete Provider.ts

--- src/Provider.ts
+++
@@ -1,7 +0,0 @@
-export { SkriptCompletionItemProvider } from './provider/SkriptCompletionItemProvider';
-export { SkriptDefinitionProvider } from './provider/SkriptDefinitionProvider';
-export { SkriptDocumentColorProvider } from './provider/SkriptDocumentColorProvider';
-export { SkriptDocumentSymbolProvider } from './provider/SkriptDocumentSymbolProvider';
-export { SkriptHoverProvider } from './provider/SkriptHoverProvider';
-export { SkriptWorkspaceSymbolProvider } from './provider/SkriptWorkspaceSymbolProvider';
-export { SkriptDocumentSemanticTokensProvider } from './provider/SkriptDocumentSemanticTokensProvider';
